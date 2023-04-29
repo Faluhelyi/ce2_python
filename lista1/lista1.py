a39d8f4eff6898f98bc3f829a060b1e854ba95ea
Faluhelyi <chalizard000@gmail.com>
2023-04-29 18:17:42 -0300

    Update files

---
+++ lista1/lista1.py
@@ -0,0 +1,59 @@
+class lista1_1:
+    def __init__(self, x0: float, tol: float):
+        """
+        Get hyperparams
+        """
+        self.x0 = x0
+        self.tol = tol
+
+    def get_root(self, f, df) -> float:
+        '''
+        Get a root of a given function by Newthon-Raphson method
+
+        args:
+        -------------------------------------
+        f <- function\n
+        df <- derivative of f\n
+        x0 <- inicial guess\n
+        tol <- error tolerance\n
+        '''
+        x0 = self.x0
+        tol = self.tol
+        if abs(f(x0)) < tol:
+            return x0
+
+        else:
+            self.x0 = x0 - (f(x0)/df(x0)) if df(x0) != 0 else None
+            x0 = self.x0
+            return lista1_1.get_root(self, f, df) if x0 != None else None
+        
+    def get_root2(self, f, df):
+        pass
+        
+print("------------- solucao (a) ---------------------------------------")
+############################################
+## sqrt(2) pelo metodo de Newthon-Raphson ##
+############################################
+r11 = lista1_1(x0= 1, tol= 1e-3).get_root(f = lambda x: (x**2) -2, df= lambda x: 2*x)
+r12 = lista1_1(x0= 1, tol= 1e-10).get_root(f = lambda x: (x**2) -2, df= lambda x: 2*x)
+
+print(f"sqrt(2) por Newthon-Raphson (tol = 1e-3):\n x0 = 1: {round(r11, 9)}")
+print(f"sqrt(2) por Newthon-Raphson (tol = 1e-10):\n x0 = 1: {round(r12, 9)}")
+
+print("------------- solucao (b) ---------------------------------------")
+###############################################################
+## Raizes de f(x) = (x**2) - 9 pelo metodo de Newton-Raphson ##
+###############################################################
+r21 = lista1_1(x0= -4, tol= 1e-3).get_root(f = lambda x: (x**2) -9, df= lambda x: 2*x)
+r22 = lista1_1(x0= 0, tol= 1e-3).get_root(f = lambda x: (x**2) -9, df= lambda x: 2*x)
+r23 = lista1_1(x0= 4, tol= 1e-3).get_root(f = lambda x: (x**2) -9, df= lambda x: 2*x)
+
+r24 = lista1_1(x0= -4, tol= 1e-10).get_root(f = lambda x: (x**2) -9, df= lambda x: 2*x)
+r25 = lista1_1(x0= 0, tol= 1e-10).get_root(f = lambda x: (x**2) -9, df= lambda x: 2*x)
+r26 = lista1_1(x0= 4, tol= 1e-10).get_root(f = lambda x: (x**2) -9, df= lambda x: 2*x)
+
+print(f"Raizes pelo metodo de Newton-Raphson (tol = 1e-3):\n x0 = -4: {r21}\n x0 = 0: {r22}\n x0 = 4: {r23}")
+print(f"Raizes pelo metodo de Newton-Raphson (tol = 1e-10):\n x0 = -4: {r24}\n x0 = 0: {r25}\n x0 = 4: {r26}")
+
+
+
