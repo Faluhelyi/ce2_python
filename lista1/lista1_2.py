119e978d1970de09361a228af9bc8a15676f8604
Faluhelyi <chalizard000@gmail.com>
2023-05-01 01:29:18 -0300

    Update files

---
+++ lista1/lista1_2.py
@@ -0,0 +1,46 @@
+class lista1_2:
+    def __init__(self, n: int):
+        """
+        Get hyperparams
+        """
+        self.n = n
+
+    def estimate_area_unit_circ(self):
+        """
+        function to estimate the area of an unit circle by Monte Carlo
+        """
+        import random
+        num_inside = 0
+        for i in range(self.n):
+            x = random.uniform(-1, 1)
+            y = random.uniform(-1, 1)
+            if x**2 + y**2 <= 1:
+                num_inside = num_inside + 1
+        return 4*(num_inside/self.n)
+    
+    def estimate_area_unit_circ2(self):
+        import random
+        num_inside = 0
+        for i in range(self.n):
+            x =  random.randint(-1 ,1)
+            y =  random.randint(-1 ,1)
+            if x**2 + y**2 <= 1:
+                num_inside = num_inside + 1
+        return 4*(num_inside/self.n)
+    
+
+import numpy as np
+
+print("------------------------------ solucao algoritmo 1 ------------------------------")
+r11 = lista1_2(1_000).estimate_area_unit_circ()
+r12 = lista1_2(500_000).estimate_area_unit_circ()
+print(f"Estimativa da area do circulo unitario por Monte Carlo: \n1_000 repeticoes: Area ~= {r11},\
+      Abs. error ~={abs(r11-np.pi)} \n500_000 repeticoes: Area ~= {r12},\
+      Abs. error ~={abs(r12-np.pi)}")
+print("------------------------------ solucao algoritmo 2 ------------------------------")
+r11 = lista1_2(1_000).estimate_area_unit_circ2()
+r12 = lista1_2(500_000).estimate_area_unit_circ2()
+print(f"Estimativa da area do circulo unitario por Monte Carlo: \n1_000 repeticoes: Area ~= {r11},\
+      Abs. error ~={abs(r11-np.pi)} \n500_000 repeticoes: Area ~= {r12},\
+      Abs. error ~={abs(r12-np.pi)}")
+
---
+++ lista1/lista1_3.py
@@ -0,0 +1,11 @@
+class lista1_3:
+    def __init__(self, n: int):
+        self.n = n
+    
+    def factorial(self):
+        pass
+
+    def pascal_t(self):
+        pass
+
+
